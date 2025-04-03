eb3f0cc36a184bf9d68bdd62df6755b26debc49e
Alem Tuzlak <t.zlak@hotmail.com>
2025-04-03 18:41:25 +0200

    remix hook form added

--- app/routes/users+/$username_+/__note-editor.tsx
+++ app/routes/users+/$username_+/__note-editor.tsx
@@ -1,16 +1,23 @@
 import {
+	type FieldMetadata,
 	FormProvider,
 	getFieldsetProps,
 	getFormProps,
 	getInputProps,
 	getTextareaProps,
 	useForm,
-	type FieldMetadata,
 } from '@conform-to/react'
 import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Img } from 'openimg/react'
 import { useState } from 'react'
+import { useFieldArray } from 'react-hook-form'
 import { Form } from 'react-router'
+import {
+	RemixFormProvider,
+	useRemixForm,
+	useRemixFormContext,
+} from 'remix-hook-form'
 import { z } from 'zod'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
@@ -21,7 +28,7 @@ import { Label } from '#app/components/ui/label.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { cn, getNoteImgSrc, useIsPending } from '#app/utils/misc.tsx'
-import { type Info } from './+types/notes.$noteId_.edit.ts'
+import type { Info } from './+types/notes.$noteId_.edit.ts'
 
 const titleMinLength = 1
 const titleMaxLength = 100
@@ -38,7 +45,7 @@ const ImageFieldsetSchema = z.object({
 		.refine((file) => {
 			return !file || file.size <= MAX_UPLOAD_SIZE
 		}, 'File size must be less than 3MB'),
-	altText: z.string().optional(),
+	altText: z.string().optional().nullable(),
 })
 
 export type ImageFieldset = z.infer<typeof ImageFieldsetSchema>
@@ -50,37 +57,31 @@ export const NoteEditorSchema = z.object({
 	images: z.array(ImageFieldsetSchema).max(5).optional(),
 })
 
-export function NoteEditor({
-	note,
-	actionData,
-}: {
-	note?: Info['loaderData']['note']
-	actionData?: Info['actionData']
-}) {
+type NoteEditor = z.infer<typeof NoteEditorSchema>
+
+const resolver = zodResolver(NoteEditorSchema)
+
+export function NoteEditor({ note }: { note?: Info['loaderData']['note'] }) {
 	const isPending = useIsPending()
 
-	const [form, fields] = useForm({
-		id: 'note-editor',
-		constraint: getZodConstraint(NoteEditorSchema),
-		lastResult: actionData?.result,
-		onValidate({ formData }) {
-			return parseWithZod(formData, { schema: NoteEditorSchema })
-		},
-		defaultValue: {
+	const form = useRemixForm({
+		resolver,
+		defaultValues: {
 			...note,
 			images: note?.images ?? [{}],
 		},
-		shouldRevalidate: 'onBlur',
+		reValidateMode: 'onBlur',
 	})
-	const imageList = fields.images.getFieldList()
+	const { handleSubmit, watch, formState, register, setValue } = form
+	const imageList = watch('images')
 
 	return (
 		<div className="absolute inset-0">
-			<FormProvider context={form.context}>
+			<RemixFormProvider {...form}>
 				<Form
 					method="POST"
 					className="flex h-full flex-col gap-y-4 overflow-x-hidden overflow-y-auto px-10 pt-12 pb-28"
-					{...getFormProps(form)}
+					onSubmit={handleSubmit}
 					encType="multipart/form-data"
 				>
 					{/*
@@ -95,33 +96,31 @@ export function NoteEditor({
 							labelProps={{ children: 'Title' }}
 							inputProps={{
 								autoFocus: true,
-								...getInputProps(fields.title, { type: 'text' }),
+								...register('title'),
 							}}
-							errors={fields.title.errors}
+							errors={[formState.errors.content?.message]}
 						/>
 						<TextareaField
 							labelProps={{ children: 'Content' }}
 							textareaProps={{
-								...getTextareaProps(fields.content),
+								...register('content'),
 							}}
-							errors={fields.content.errors}
+							errors={[formState.errors.content?.message]}
 						/>
 						<div>
 							<Label>Images</Label>
 							<ul className="flex flex-col gap-4">
-								{imageList.map((imageMeta, index) => {
+								{imageList?.map((imageMeta, index) => {
 									const image = note?.images[index]
 									return (
 										<li
-											key={imageMeta.key}
+											key={imageMeta.id}
 											className="border-muted-foreground relative border-b-2"
 										>
 											<button
 												className="text-foreground-destructive absolute top-0 right-0"
-												{...form.remove.getButtonProps({
-													name: fields.images.name,
-													index,
-												})}
+												onClick={() => setValue('images', imageList)}
+												type="button"
 											>
 												<span aria-hidden>
 													<Icon name="cross-1" />
@@ -139,24 +138,24 @@ export function NoteEditor({
 								})}
 							</ul>
 						</div>
-						<Button
-							className="mt-3"
-							{...form.insert.getButtonProps({ name: fields.images.name })}
-						>
+						<Button className="mt-3">
 							<span aria-hidden>
 								<Icon name="plus">Image</Icon>
 							</span>{' '}
 							<span className="sr-only">Add image</span>
 						</Button>
 					</div>
-					<ErrorList id={form.errorId} errors={form.errors} />
+					<ErrorList
+						errors={[
+							...(formState.errors.images?.map((e) => e?.message ?? '') ?? []),
+						]}
+					/>
 				</Form>
 				<div className={floatingToolbarClassName}>
-					<Button variant="destructive" {...form.reset.getButtonProps()}>
+					<Button variant="destructive" onClick={() => form.reset()}>
 						Reset
 					</Button>
 					<StatusButton
-						form={form.id}
 						type="submit"
 						disabled={isPending}
 						status={isPending ? 'pending' : 'idle'}
@@ -164,7 +163,7 @@ export function NoteEditor({
 						Submit
 					</StatusButton>
 				</div>
-			</FormProvider>
+			</RemixFormProvider>
 		</div>
 	)
 }
@@ -176,12 +175,20 @@ function ImageChooser({
 	meta: FieldMetadata<ImageFieldset>
 	objectKey: string | undefined
 }) {
-	const fields = meta.getFieldset()
+	/* 	const fields = meta.getFieldset()
 	const existingImage = Boolean(fields.id.initialValue)
 	const [previewImage, setPreviewImage] = useState<string | null>(
 		objectKey ? getNoteImgSrc(objectKey) : null,
 	)
 	const [altText, setAltText] = useState(fields.altText.initialValue ?? '')
+ */
+	const form = useRemixFormContext<NoteEditor>()
+	const { fields } = useFieldArray({ name: 'images', control: form.control })
+	const existingImage = Boolean(fields?.length)
+	const [previewImage, setPreviewImage] = useState<string | null>(
+		objectKey ? getNoteImgSrc(objectKey) : null,
+	)
+	const [altText, setAltText] = useState(fields?.[0]?.altText ?? '')
 
 	return (
 		<fieldset {...getFieldsetProps(meta)}>
@@ -189,7 +196,8 @@ function ImageChooser({
 				<div className="w-32">
 					<div className="relative size-32">
 						<label
-							htmlFor={fields.file.id}
+							//htmlFor={fields.file.id}
+							htmlFor={fields?.[0]?.id}
 							className={cn('group absolute size-32 rounded-lg', {
 								'bg-accent opacity-40 focus-within:opacity-100 hover:opacity-100':
 									!previewImage,
